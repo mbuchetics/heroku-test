f2528fcc33ec1b73d07d4c7c6a8997a1e1051a74
Matthias Buchetics <mbuchetics@gmail.com>
2012-01-13 10:30:28 +0100

    more detailed version info

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-console.log('node version: ' + process.version);
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(req, res) {
+    var message = 'Heroku Test\n\n';
+    message += '<code><pre>';
+    message += require('util').inspect(process.versions, true, null);
+    message += '</pre></code>';
+    
+    res.send(message);
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Running on " + process.version);
+    console.log("Listening on " + port);
+});
