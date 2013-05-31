322a75f9548b12a891d46fbea44693a9e5a18491
Matthias Buchetics <mbuchetics@gmail.com>
2013-05-31 10:25:27 +0200

    updated test

--- grunt.js
+++ grunt.js
@@ -3,5 +3,15 @@ module.exports = function(grunt) {
         grunt.log.writeln('oh, hello!');
     });
 
+    grunt.registerTask('hello-prod', 'Hello World ...', function() {
+        grunt.log.writeln('oh, hello production!');
+    });
+
+    grunt.registerTask('hello-dev', 'Hello World ...', function() {
+        grunt.log.writeln('oh, hello dev!');
+    });
+
     grunt.registerTask('heroku', 'hello');
+    grunt.registerTask('heroku:prod', 'hello-prod');
+    grunt.registerTask('heroku:dev', 'hello-dev');
 };
