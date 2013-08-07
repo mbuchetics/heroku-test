7fa7eb6858076846c16c570b6a128679a18b7696
Matthias Buchetics <mbuchetics@gmail.com>
2013-08-07 16:36:54 +0200

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,5 @@ module.exports = function(grunt) {
         grunt.log.writeln('oh, hello!');
     });
 
-    grunt.registerTask('heroku', 'hello');
+    grunt.registerTask('heroku:production', 'hello');
 };
